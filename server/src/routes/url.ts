04b3ce1fffeddb96888c1ed490a239c42bdbc0bf
Ayyappa Swamy Angadi <ayyappaswamyangadi@Ayyappas-Laptop.local>
2025-08-01 18:43:48 +0530

    test: verified POST /api/shorten with Postman

--- server/src/routes/url.ts
+++ server/src/routes/url.ts
@@ -1,9 +1,34 @@
-import express from "express";
+import Router from "express";
+import { nanoid } from "nanoid";
+import Url from "../models/Url";
 
-const router = express.Router();
+const router = Router();
 
-router.get("/test", (_, res) => {
-  res.send("Router is working");
+const BASE_URL = process.env.BASE_URL || "http://localhost:3000";
+
+router.post("/shorten", async (request, response) => {
+  const { originalUrl } = request.body;
+
+  // no valid originalUrl
+  if (!originalUrl || typeof originalUrl !== "string") {
+    return response.status(400).json({ error: "Invalid url" });
+  }
+  try {
+    //if short id exists
+    const existing = await Url.findOne({ originalUrl });
+    if (existing) {
+      return response?.json({ shortUrl: `${BASE_URL}/${existing.shortId}` });
+    }
+
+    // if no short id exists
+    const shortId = nanoid(6);
+
+    const newUrl = new Url({ originalUrl, shortId });
+    await newUrl.save();
+    response.status(201).json({ shortUrl: `${BASE_URL}/${shortId}` });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 export default router;
